bb9813987ae70327eedebffa8426107d8d2bfc96
Ming Shen <sshen@myevergreen.com>
2025-05-26 23:15:14 -0400

    add create2

--- tbb_roomba/tbb_roomba/pycreate2/OI.py
+++ tbb_roomba/tbb_roomba/pycreate2/OI.py
@@ -5,6 +5,7 @@
 ##############################################
 # I took some of these ideas from: https://bitbucket.org/lemoneer/irobot
 
+
 # could replace with:
 #   class LEDS(object): DEBRIS=0x01; SPOT=0x02; DOCK=0x04; CHECK_ROBOT=0x08
 class Namespace(object):
@@ -12,24 +13,77 @@ class Namespace(object):
         self.__dict__.update(kwds)
 
 
-BAUD_RATE           = Namespace(BAUD_300=0, BAUD_600=1, BAUD_1200=2, BAUD_2400=3, BAUD_4800=4, BAUD_9600=5, BAUD_14400=6, BAUD_19200=7, BAUD_28800=8, BAUD_38400=9, BAUD_57600=10, BAUD_115200=11, DEFAULT=11)
-DAYS                = Namespace(SUNDAY=0x01, MONDAY=0x02, TUESDAY=0x04, WEDNESDAY=0x08, THURSDAY=0x10, FRIDAY=0x20, SATURDAY=0x40)
-DRIVE               = Namespace(STRAIGHT=0x8000, STRAIGHT_ALT=0x7FFF, TURN_CW=-1, TURN_CCW=0x0001)
-MOTORS              = Namespace(SIDE_BRUSH=0x01, VACUUM=0x02, MAIN_BRUSH=0x04, SIDE_BRUSH_DIRECTION=0x08, MAIN_BRUSH_DIRECTION=0x10)
-LEDS                = Namespace(DEBRIS=0x01, SPOT=0x02, DOCK=0x04, CHECK_ROBOT=0x08)
+BAUD_RATE = Namespace(
+    BAUD_300=0,
+    BAUD_600=1,
+    BAUD_1200=2,
+    BAUD_2400=3,
+    BAUD_4800=4,
+    BAUD_9600=5,
+    BAUD_14400=6,
+    BAUD_19200=7,
+    BAUD_28800=8,
+    BAUD_38400=9,
+    BAUD_57600=10,
+    BAUD_115200=11,
+    DEFAULT=11,
+)
+DAYS = Namespace(
+    SUNDAY=0x01,
+    MONDAY=0x02,
+    TUESDAY=0x04,
+    WEDNESDAY=0x08,
+    THURSDAY=0x10,
+    FRIDAY=0x20,
+    SATURDAY=0x40,
+)
+DRIVE = Namespace(STRAIGHT=0x8000, STRAIGHT_ALT=0x7FFF, TURN_CW=-1, TURN_CCW=0x0001)
+MOTORS = Namespace(
+    SIDE_BRUSH=0x01,
+    VACUUM=0x02,
+    MAIN_BRUSH=0x04,
+    SIDE_BRUSH_DIRECTION=0x08,
+    MAIN_BRUSH_DIRECTION=0x10,
+)
+LEDS = Namespace(DEBRIS=0x01, SPOT=0x02, DOCK=0x04, CHECK_ROBOT=0x08)
 # WEEKDAY_LEDS        = Namespace(SUNDAY=0x01, MONDAY=0x02, TUESDAY=0x04, WEDNESDAY=0x08, THURSDAY=0x10, FRIDAY=0x20, SATURDAY=0x40)
-WEEKDAY_LEDS        = DAYS
-SCHEDULING_LEDS     = Namespace(COLON=0x01, PM=0x02, AM=0x04, CLOCK=0x08, SCHEDULE=0x10)
-RAW_LED             = Namespace(A=0x01, B=0x02, C=0x04, D=0x08, E=0x10, F=0x20, G=0x40)
-BUTTONS             = Namespace(CLEAN=0x01, SPOT=0x02, DOCK=0x04, MINUTE=0x08, HOUR=0x10, DAY=0x20, SCHEDULE=0x40, CLOCK=0x80)
-ROBOT               = Namespace(TICK_PER_REV=508.8, WHEEL_DIAMETER=72, WHEEL_BASE=235, TICK_TO_DISTANCE=0.44456499814949904317867595046408)
-MODES               = Namespace(OFF=0, PASSIVE=1, SAFE=2, FULL=3)
-WHEEL_OVERCURRENT   = Namespace(SIDE_BRUSH=0x01, MAIN_BRUSH=0x02, RIGHT_WHEEL=0x04, LEFT_WHEEL=0x08)
-BUMPS_WHEEL_DROPS   = Namespace(BUMP_RIGHT=0x01, BUMP_LEFT=0x02, WHEEL_DROP_RIGHT=0x04, WHEEL_DROP_LEFT=0x08)
-CHARGE_SOURCE       = Namespace(INTERNAL=0x01, HOME_BASE=0x02)
-LIGHT_BUMPER        = Namespace(LEFT=0x01, FRONT_LEFT=0x02, CENTER_LEFT=0x04, CENTER_RIGHT=0x08, FRONT_RIGHT=0x10, RIGHT=0x20)
-STASIS              = Namespace(TOGGLING=0x01, DISABLED=0x02)
-OPCODES             = Namespace(
+WEEKDAY_LEDS = DAYS
+SCHEDULING_LEDS = Namespace(COLON=0x01, PM=0x02, AM=0x04, CLOCK=0x08, SCHEDULE=0x10)
+RAW_LED = Namespace(A=0x01, B=0x02, C=0x04, D=0x08, E=0x10, F=0x20, G=0x40)
+BUTTONS = Namespace(
+    CLEAN=0x01,
+    SPOT=0x02,
+    DOCK=0x04,
+    MINUTE=0x08,
+    HOUR=0x10,
+    DAY=0x20,
+    SCHEDULE=0x40,
+    CLOCK=0x80,
+)
+ROBOT = Namespace(
+    TICK_PER_REV=508.8,
+    WHEEL_DIAMETER=72,
+    WHEEL_BASE=235,
+    TICK_TO_DISTANCE=0.44456499814949904317867595046408,
+)
+MODES = Namespace(OFF=0, PASSIVE=1, SAFE=2, FULL=3)
+WHEEL_OVERCURRENT = Namespace(
+    SIDE_BRUSH=0x01, MAIN_BRUSH=0x02, RIGHT_WHEEL=0x04, LEFT_WHEEL=0x08
+)
+BUMPS_WHEEL_DROPS = Namespace(
+    BUMP_RIGHT=0x01, BUMP_LEFT=0x02, WHEEL_DROP_RIGHT=0x04, WHEEL_DROP_LEFT=0x08
+)
+CHARGE_SOURCE = Namespace(INTERNAL=0x01, HOME_BASE=0x02)
+LIGHT_BUMPER = Namespace(
+    LEFT=0x01,
+    FRONT_LEFT=0x02,
+    CENTER_LEFT=0x04,
+    CENTER_RIGHT=0x08,
+    FRONT_RIGHT=0x10,
+    RIGHT=0x20,
+)
+STASIS = Namespace(TOGGLING=0x01, DISABLED=0x02)
+OPCODES = Namespace(
     RESET=7,
     OI_MODE=35,
     START=128,
@@ -59,18 +113,75 @@ OPCODES             = Namespace(
     # BUTTONS=165,
     # SCHEDULE=167,
     # SET_DAY_TIME=168,
-    STOP=173
+    STOP=173,
 )
 
 RESPONSE_SIZES = {
-    0: 26, 1: 10, 2: 6, 3: 10, 4: 14, 5: 12, 6: 52,
+    0: 26,
+    1: 10,
+    2: 6,
+    3: 10,
+    4: 14,
+    5: 12,
+    6: 52,
     # actual sensors
-    7: 1, 8: 1, 9: 1, 10: 1, 11: 1, 12: 1, 13: 1, 14: 1, 15: 1, 16: 1, 17: 1, 18: 1, 19: 2, 20: 2, 21: 1,
-    22: 2, 23: 2, 24: 1, 25: 2, 26: 2, 27: 2, 28: 2, 29: 2, 30: 2, 31: 2, 32: 3, 33: 3, 34: 1, 35: 1,
-    36: 1, 37: 1, 38: 1, 39: 2, 40: 2, 41: 2, 42: 2, 43: 2, 44: 2, 45: 1, 46: 2, 47: 2, 48: 2, 49: 2,
-    50: 2, 51: 2, 52: 1, 53: 1, 54: 2, 55: 2, 56: 2, 57: 2, 58: 1,
+    7: 1,
+    8: 1,
+    9: 1,
+    10: 1,
+    11: 1,
+    12: 1,
+    13: 1,
+    14: 1,
+    15: 1,
+    16: 1,
+    17: 1,
+    18: 1,
+    19: 2,
+    20: 2,
+    21: 1,
+    22: 2,
+    23: 2,
+    24: 1,
+    25: 2,
+    26: 2,
+    27: 2,
+    28: 2,
+    29: 2,
+    30: 2,
+    31: 2,
+    32: 3,
+    33: 3,
+    34: 1,
+    35: 1,
+    36: 1,
+    37: 1,
+    38: 1,
+    39: 2,
+    40: 2,
+    41: 2,
+    42: 2,
+    43: 2,
+    44: 2,
+    45: 1,
+    46: 2,
+    47: 2,
+    48: 2,
+    49: 2,
+    50: 2,
+    51: 2,
+    52: 1,
+    53: 1,
+    54: 2,
+    55: 2,
+    56: 2,
+    57: 2,
+    58: 1,
     # end actual sensors
-    100: 80, 101: 28, 106: 12, 107: 9
+    100: 80,
+    101: 28,
+    106: 12,
+    107: 9,
 }
 
 
@@ -82,12 +193,12 @@ def calc_query_data_len(pkts):
 
 
 CHARGING_STATE = {
-    0: 'not charging',
-    1: 'reconditioning charging',
-    2: 'full charging',
-    3: 'trickle charging',
-    4: 'waiting',
-    5: 'charging fault condition'
+    0: "not charging",
+    1: "reconditioning charging",
+    2: "full charging",
+    3: "trickle charging",
+    4: "waiting",
+    5: "charging fault condition",
 }
 
 
@@ -191,7 +302,7 @@ MIDI_TABLE = {
     "D4": 62,
     "D5": 74,
     "D2": 38,
-    "D3": 50
+    "D3": 50,
 }
 
 REMOTE_OPCODES = {
@@ -228,5 +339,5 @@ REMOTE_OPCODES = {
     250: "red-buoy-and-force-field",
     252: "red-buoy-and-green-buoy",
     254: "red-buoy-and-green-buoy-and-force-field",
-    255: "none"
+    255: "none",
 }
--- tbb_roomba/tbb_roomba/tbb_roomba_node.py
+++ tbb_roomba/tbb_roomba/tbb_roomba_node.py
@@ -4,10 +4,12 @@ from rclpy.node import Node
 class TbbRoombaNode(Node):
     def __init__(self):
         super().__init__('tbb_roomba_node')
+        self.get_logger().info('TBB Roomba Node has been started.')
 
 def main(args=None):
     rclpy.init(args=args)
     tbb_roomba_node = TbbRoombaNode()
+    
     rclpy.spin(tbb_roomba_node)
     rclpy.shutdown()
 
