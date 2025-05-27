1cf1bb29ed30c15627cbe63bb450144f6aa059ed
Ming Shen <sshen@myevergreen.com>
2025-05-27 11:09:01 -0400

    remove cpp config

--- tbb_roomba/tbb_roomba/tbb_roomba_node.py
+++ tbb_roomba/tbb_roomba/tbb_roomba_node.py
@@ -1,10 +1,12 @@
 import rclpy
 from rclpy.node import Node
+from pycreate2 import Create2
 
 class TbbRoombaNode(Node):
     def __init__(self):
         super().__init__('tbb_roomba_node')
         self.get_logger().info('TBB Roomba Node has been started.')
+        self.roomba = Create2('/dev/ttyUSB0')
 
 def main(args=None):
     rclpy.init(args=args)
